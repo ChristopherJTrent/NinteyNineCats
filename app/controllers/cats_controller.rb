085b0a302b73bbacec86996d367ac063f177acab
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-02-16 14:55:16 -0800

    complete form partial

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,4 +1,5 @@
 class CatsController < ApplicationController
+  # * section: user interface
   def index
     @cats = Cat.all
     render :index
@@ -7,21 +8,25 @@ class CatsController < ApplicationController
     @cat = Cat.find_by(id: params[:id])
     render :show
   end
-  def update
-  
-  end
-  def create
-
-  end
   def new
 
   end
   def edit
 
   end
+
+  # * section: API
+  def update
+  
+  end
+  def create
+    @cat = Cat.create!(cat_params)
+    render :show 
+  end
+
   private
     # Only allow a list of trusted parameters through.
     def cat_params
-      params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+      params.require(:cats).permit(:birth_date, :color, :name, :sex, :description)
     end
 end
